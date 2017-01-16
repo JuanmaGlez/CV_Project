1704bb6c2f93b7a2f519daf633e4e7f1ceb25a8f
JuanmaGlez <smigol_82@yahoo.es>
2017-01-16 11:39:11 +0100

    comentarios añadidos al código

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,8 @@
+/**
+ * Muestra por pantalla la frase "¡Hola Mundo!"
+ *
+ * @author Juanma
+ */
 public class Hola Mundo {
 	public static void main(String[] args) {
 		System.out.println("¡Hola Mundo!");

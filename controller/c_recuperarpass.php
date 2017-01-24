7a05b5f3bbe518fec6f90d72da4ceb53f4084592
JuanmaGlez <smigol_82@yahoo.es>
2017-01-24 12:26:18 +0100

    modificado

--- controller/c_recuperarpass.php
+++ controller/c_recuperarpass.php
@@ -20,16 +20,20 @@ class Prueba
   }
 
   public function enviado() {
-    $mail = "Prueba de mensaje";
+    // Mensaje
+    $mensaje = "Link para poder recuperar su password";
     //Titulo
-    $titulo = "PRUEBA DE TITULO";
+    $titulo = "Recuperar Password";
     //cabecera
-    $headers = "MIME-Version: 1.0\r\n";
+  /*  $headers = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
     //dirección del remitente
-    $headers .= "From: Curriculum Web < jmgonzalez@comvive.es >\r\n";
+    $headers .= "From: Curriculum Web < jmgonzalez@comvive.es >\r\n";*/
+    $cabeceras = 'From: jmgonzalez@comvive.es' . "\r\n" .
+    'Reply-To: jmgonzalez@comvive.es' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
     //Enviamos el mensaje a tu_dirección_email
-    $bool = mail($this->email,$titulo,$mail,$headers);
+    $bool = mail($this->email,$titulo,$mensaje,$cabeceras);
     if($bool){
       echo "Mensaje enviado";
     }else{

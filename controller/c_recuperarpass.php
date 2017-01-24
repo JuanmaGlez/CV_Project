c0f43d70ee6fc465d84e3f5e3d45985821becdb2
JuanmaGlez <smigol_82@yahoo.es>
2017-01-24 12:47:29 +0100

    modificado

--- controller/c_recuperarpass.php
+++ controller/c_recuperarpass.php
@@ -8,10 +8,7 @@ class Prueba
   public $email;
 
   public function __construct($email=null) {
-    if ($_POST['correo'] == ""){
-      $this->email="Esta vacio";
-    } else {
-      //$this->email=$_POST['correo'];
+    if ($_POST['correo'] != ""){
       echo $email;
       $this->email=$email;
     }
@@ -22,24 +19,28 @@ class Prueba
   }
 
   public function enviado() {
-    // Mensaje
-    $mensaje = "Link para poder recuperar su password";
-    //Titulo
-    $titulo = "Recuperar Password";
-    //cabecera
-  /*  $headers = "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-    //dirección del remitente
-    $headers .= "From: Curriculum Web < jmgonzalez@comvive.es >\r\n";*/
-    $cabeceras = 'From: jmgonzalez@comvive.es' . "\r\n" .
-    'Reply-To: jmgonzalez@comvive.es' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    //Enviamos el mensaje a tu_dirección_email
-    $bool = mail($this->email,$titulo,$mensaje,$cabeceras);
-    if($bool){
-      echo "Mensaje enviado";
-    }else{
-      echo "Mensaje no enviado";
+    if ($this->email!="") {
+      // Mensaje
+      $mensaje = "Link para poder recuperar su password";
+      //Titulo
+      $titulo = "Recuperar Password";
+      //cabecera
+      /*  $headers = "MIME-Version: 1.0\r\n";
+      $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+      //dirección del remitente
+      $headers .= "From: Curriculum Web < jmgonzalez@comvive.es >\r\n";*/
+      $cabeceras = 'From: jmgonzalez@comvive.es' . "\r\n" .
+      'Reply-To: jmgonzalez@comvive.es' . "\r\n" .
+      'X-Mailer: PHP/' . phpversion();
+      //Enviamos el mensaje a tu_dirección_email
+      $bool = mail($this->email,$titulo,$mensaje,$cabeceras);
+      if($bool){
+        echo "Mensaje enviado";
+      }else{
+        echo "Mensaje no enviado";
+      }
+    } else {
+      echo "Caja vacia";
     }
   }
 

2a9de7fdef95b20e38ffea0ec6c4d02d7495a6a9
JuanmaGlez <smigol_82@yahoo.es>
2017-01-24 10:25:19 +0100

    modificado

--- model/Usuarios.php
+++ model/Usuarios.php
@@ -256,7 +256,7 @@
 
     //Método Desactivar Cuenta Usuario
     public function desactivarCuenta($valor,$nombre){
-      $sql="UPDATE usuarios set desactivar = '$valor' where username='$nombre'";
+      $sql="UPDATE usuarios set desactivar = $valor where username='$nombre'";
       $valorDesactivar=$this->conectarse->query($sql);
       if ($valorDesactivar){
         echo "Usuario desactivado";

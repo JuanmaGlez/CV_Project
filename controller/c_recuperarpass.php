84a4f572e0899ed3708fb5709904a8f19b3f2918
JuanmaGlez <smigol_82@yahoo.es>
2017-01-24 12:19:51 +0100

    modificado

--- controller/c_recuperarpass.php
+++ controller/c_recuperarpass.php
@@ -27,7 +27,7 @@ class Prueba
     $headers = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
     //dirección del remitente
-    $headers .= "From: Curriculum Web < recuperarpassword@curriculumweb.com >\r\n";
+    $headers .= "From: Curriculum Web < jmgonzalez@comvive.es >\r\n";
     //Enviamos el mensaje a tu_dirección_email
     $bool = mail($this->email,$titulo,$mail,$headers);
     if($bool){

0c676a49a21da6588ea50f7172098c328a841aa0
JuanmaGlez <smigol_82@yahoo.es>
2017-01-24 11:31:46 +0100

    modificado

--- controller/c_perfil.php
+++ controller/c_perfil.php
@@ -65,8 +65,13 @@ class Perfil {
 
   public function mostrar($nombre){
     $datosRecuperados=$this->objUser->mostrarUsuario($nombre);
+    if ($datosRecupados['desactivado'] == 0) {
+      $datos=$datosRecupados['desactivado'];
+    } else {
+      $datos=$datosRecupados['desactivado'];
+    }
     echo $datosRecuperados['idUsuario'] . " " . $datosRecuperados['username'] . " " . $datosRecuperados['email'] . " " . $datosRecuperados['name'] . " " . $datosRecuperados['surname']
-      . " " . $datosRecuperados['idTipoUsuario'];
+      . " " . $datosRecuperados['idTipoUsuario'] . " " . $datos;
   }
 
   public function desactivo($valor,$nombre){

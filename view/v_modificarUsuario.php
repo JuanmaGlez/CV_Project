de761204d5008f544a538ac7fa1669d33b828a8c
JuanmaGlez <smigol_82@yahoo.es>
2017-01-25 09:18:18 +0100

    modificados

--- view/v_modificarUsuario.php
+++ view/v_modificarUsuario.php
@@ -2,8 +2,8 @@
 session_start();
 require_once ("/furanet/sites/jmgonzalez.com/web/htdocs/CV_Project/controller/c_modificarDatos.php");
 require_once ("../controller/c_perfil.php");
-$objVerLog=new Perfil($_POST['username'],$_POST['password']);
-$objVerLog->checkConexion();
+/*$objVerLog=new Perfil($_POST['username'],$_POST['password']);
+$objVerLog->checkConexion();*/
 
 $objmodificar=new Modificar();
 if(isset($_POST["submit"])){

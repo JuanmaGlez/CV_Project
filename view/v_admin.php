0547e16711105d4b40f23b95d1c24346216ddc9e
JuanmaGlez <smigol_82@yahoo.es>
2017-01-26 10:10:30 +0100

    modificado

--- view/v_admin.php
+++ view/v_admin.php
@@ -11,11 +11,6 @@ $objAdmin=new Admin($_POST["usuarios"]);
    <select class='lista' name='usuarios'>
      <option value="0">Usuarios</option>
        <?php $objAdmin->listar(); ?>
-       <optgroup label="Opciones">
-         <option>Casillas de verificación</option>
-         <option>Botones radio</option>
-         <option>Listas</option>
-       </optgroup>
    </select>
    <input type='submit' name='ver' value='Ver'class='btn btn-success'/>
    <input type='submit' name='modificar' value='Modificar'class='btn btn-success'/>
@@ -26,13 +21,15 @@ $objAdmin=new Admin($_POST["usuarios"]);
      $objAdmin->mostrar($_POST["usuarios"]);
      echo "<br>";
      echo "<form method='post'/>";
+     echo "<input type='hidden' name='ver' value='ver'/>";
      echo "<input type='submit' name='desactivar' value='Desactivar'class='btn btn-success'/>";
      echo"</form>";
+     if (isset($_POST["desactivar"])){
+       $objAdmin->desactivar(true);
+       echo "<br>";
+     }
    }
-   if (isset($_POST["desactivar"])){
-     $objAdmin->desactivar(true);
-     echo "<br>";
-   }
+
    if (isset($_POST["modificar"])){
      $objAdmin->mostrar($_POST["usuarios"]); ?>
      <form action="<?PHP $PHP_SELF ?>" method="post" class="col-lg-5">

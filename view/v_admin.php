8bcd1cae9439b4ce89f977437a067354dfa36ba2
JuanmaGlez <smigol_82@yahoo.es>
2017-01-26 09:05:55 +0100

    modificado

--- view/v_admin.php
+++ view/v_admin.php
@@ -14,13 +14,14 @@ $objAdmin=new Admin($_POST["usuarios"]);
    </select>
    <input type='submit' name='ver' value='Ver'class='btn btn-success'/>
    <input type='submit' name='modificar' value='Modificar'class='btn btn-success'/>
-   <input type='submit' name='desactivar' value='Desactivar'class='btn btn-success'/>
+
  </form>
 
  <?php
    if (isset($_POST["ver"])){
      $objAdmin->mostrar();
      echo "<br>";
+     echo"<input type='submit' name='desactivar' value='Desactivar'class='btn btn-success'/>";
    }
    if (isset($_POST["desactivar"])){
      $objAdmin->desactivar(true);

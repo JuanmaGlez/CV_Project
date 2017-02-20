77b50f1e545294c4031505e46d9af2127808d4ce
JuanmaGlez <smigol_82@yahoo.es>
2017-02-20 12:57:07 +0100

    modificado

--- view/v_admin.php
+++ view/v_admin.php
@@ -9,9 +9,9 @@ $objAdmin=new Admin($_POST["usuarios"]);
  <input type="button" onclick=" location.href='v_perfil.php' " value="Volver">
  <br>
  <input type='submit' name='add' value='Añadir'class='btn btn-success'/>
-<form action="javascript:location.href=document.forms[0].elements['v_admin'].value">
+<form action="javascript:location.href=document.forms[0].elements['usuarios'].value">
  <!--<form action='v_admin.php' method='post'/>-->
-   <select onchange="location.href=this.value" class='lista' name='usuarios'>
+   <select onchange="location.href=v_admin.php" class='lista' name='usuarios'>
      <option value="0">Usuarios</option>
        <?php $objAdmin->listar(); ?>
    </select>

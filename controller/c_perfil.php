158ed032469be88eae866cb5d15fa8127560c93d
JuanmaGlez <smigol_82@yahoo.es>
2017-05-26 20:12:25 +0200

    actualizado 20170502

--- controller/c_perfil.php
+++ controller/c_perfil.php
@@ -1,94 +1,89 @@
-<?php
-/**
- *
- */
-session_start();
-require_once('/var/www/html/jobsnetworks/CV_Project/model/Usuarios.php');
+<?php 
 
-class Perfil {
-  private $usuario;
-  private $contrasena;
-  public $objUser;
+	session_start();
+	// Llamamos a la Clase Usuario
+	require_once('../model/Usuarios.php');
 
-  public function __construct($usuario,$contrasena) {
-    $this->usuario=$usuario;
-    $this->contrasena=$contrasena;
-    $this->objUser=new Usuarios($_SESSION['idUsuario']);
-  } //***Fin Construtor***
+	// Clase Perfil, donde se verifica la password del usuario entre otras cosas
+	class Perfil {
 
-  public function checkLogin(){
-    $arraybuscar=$this->objUser->buscarUsuario($this->usuario,$this->contrasena);
-    echo $arraybuscar;
-    if ($arraybuscar['desactivado'] == 0) {
-      if ($this->contrasena == $arraybuscar['password']) {
-        $_SESSION['idUsuario']=$this->objUser->getIdUsuario();
-        $_SESSION['idTipoUsuario']=$this->objUser->getIdTipos();
-        $_SESSION['loggedin'] = true;
-        $_SESSION['username']=$this->usuario;
-        $_SESSION['start'] = time();
-        $_SESSION['expire'] = $_SESSION['start'] + (10 * 60);
-        echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../view/v_perfil.php\">";
-      } else {
-          echo "Usuario o Contraseña son incorrectos.";
-          //echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../view/v_login.php\">";
-      } // ** Fin IF **
-    } else {
-        echo "Este usuario esta desactivado";
-    } // ** Fin IF **
-  } //*** Fin método checkLogin()***
+		private $usuario;
+		private $contrasena;
+		public $objetoUsuario;
+		
 
-  public function checkConexion(){
-    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    } else {
-      echo "Solo usuarios registrados.<br>";
-      echo "<br><a href='../view/v_login.php'>Login</a>";
-    exit;
-    }
+		// Método Constructor de la Clase Perfil		
+		public function __construct($usuario,$contrasena){
+			$this->usuario=$usuario;
+			$this->contrasena=$contrasena;			
+			//$this->objetoUsuario=new Usuarios($_SESSION['idUsuario']);
+			if (empty($_SESSION['idUsuario'])) {
+				$this->objetoUsuario=new Usuarios();
+			} else {
+				$this->objetoUsuario=new Usuarios($_SESSION['idUsuario']);
+			}
 
-    $ahora = time();
+		} // Fin Constructor
 
-    if ($ahora > $_SESSION['expire']) {
-      session_destroy();
-      echo "Su sesión ha terminado. <a href='../view/v_login.php'>Volver a Entrar</a>";
-      //echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../view/v_login.php\">";
-      exit;
-    }
+		// Método checkLogin
+		public function checKLogin() {
+			$datosarray=$this->objetoUsuario->buscarUsuario($this->usuario);
+			if ($datosarray != 0) {				
+				
+				if ($datosarray['estado'] == 'activado') {
+					if (password_verify($this->contrasena, $datosarray['password'])) {
+						$_SESSION['idUsuario']=$datosarray['idUsuario'];
+						$_SESSION['username']=$datosarray['username'];
+						$_SESSION['pass']=$this->contrasena;
+						$_SESSION['tipoUsuario']=$datosarray['tipoUsuario'];
+						$_SESSION['loggedin'] = true;
+						$_SESSION['start'] = time();
+		        		$_SESSION['expire'] = $_SESSION['start'] + (10 * 60);
+						echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../view/v_perfil.php\">";
+					} else {
+						echo "Usuario o Contraseña son incorrectos.";
+					} // Fin Condición verificar password
+				} else {
+					echo "Este usuario esta desactivado.";
+				} // Fin Condición estado
+			} else {
+				echo "Usuario no existe";
+			}
+		} // Fin método checkLogin
+
+		public function guardarLogin($usuario) {
+			if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+				setcookie("nombre_usuario", "$usuario", time()+86400);
+			}
+		}
+
+		public function checkConexion(){
+			if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+		    } else {
+		      echo "Solo usuarios registrados.<br>";
+		      echo "<br><a href='../view/v_login.php'>Login</a>";
+		    exit;
+		    }
+
+		    $ahora = time();
+
+		    if ($ahora > $_SESSION['expire']) {
+		      session_destroy();
+		      //echo "Su sesión ha terminado. <a href='../view/v_login.php'>Volver a Entrar</a>";
+		      echo "Su sesión ha terminado. <a href='../view/v_login.php'><input type='button' name='salir' value='Login'></a>";
+		      //echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../view/v_login.php\">";
+		      exit;
+		    }
   }
 
-/*  public function listar(){
-    $row_lista=$this->objUser->listarUsuario();
-    //$fila=$this->objUser->listarUsuario();
-    while ($fila=$row_lista->fetch_array()) {
-      if ($fila['idUsuario'] != 1){
-        echo '<option value="'.$fila['username'].'">'.$fila['username']. '</option>';
-        //echo '<option value="'.$fila.'">'.$fila. '</option>';
-      }
-    }
-  }
+		// Método cerrar sesión
+		public function closeSession(){    
+		    unset ($_SESSION['username']);
+		    session_destroy();
+		    echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../\">";
+		    exit;
+		} //Fin método cerrar sesión
 
-  public function mostrar($nombre){
-    $datosRecuperados=$this->objUser->mostrarUsuario($nombre);
-    if ($datosRecuperados['desactivado'] == 0) {
-      $datos='Activado';
-    } else {
-      $datos='Desactivado';
-    }
-    echo $datosRecuperados['idUsuario'] . " " . $datosRecuperados['username'] . " " . $datosRecuperados['email'] . " " . $datosRecuperados['name'] . " " . $datosRecuperados['surname']
-    . " " . $datosRecuperados['idTipoUsuario'] . " " . $datos;
-  }
 
-  public function desactivo($valor,$nombre){
-    $desactivo=$this->objUser->desactivarCuenta($valor,$nombre);
-  }*/
-
-  public function closeSession(){
-    //echo "saliendo";
-    unset ($_SESSION['username']);
-    session_destroy();
-    echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../view/v_login.php\">";
-    exit;
-  } //Fin método cerrar sesión
-
-} //***Fin Clase Perfil***
-
+	} // Fin de la Clase Perfil
  ?>

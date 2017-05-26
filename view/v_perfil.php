0acb065ec6f4ac1759c507092a4119ef65cf9987
JuanmaGlez <smigol_82@yahoo.es>
2017-05-26 20:27:54 +0200

    actualizado 20170523

--- view/v_perfil.php
+++ view/v_perfil.php
@@ -1,86 +1,131 @@
-<?php
-//session_start();
-require_once ("/var/www/html/jobsnetworks/CV_Project/controller/c_perfil.php");
+<?php 
 
-$objVerLog=new Perfil($_POST['username'],$_POST['password']);
-//$objVerLog->checkLogin();
-$objVerLog->checkConexion();
+  //Llamos al controlador perfil.php
+  require_once('../controller/c_perfil.php');
 
-if(isset($_POST["salir"])){
-  $objVerLog->closeSession();
-}
+  require_once("../controller/c_modificarDatos.php");
 
-//date_default_timezone_set('UTC');
-//echo date('l jS \of F Y h:i:s A');
-?>
+  require_once("../controller/c_subirFoto.php");
 
-<h1>CURRICULUM VITAE</h1>
-<h2>Perfil <?php echo $_SESSION['username']?> </h2>
-<form action="<?PHP $PHP_SELF ?>" method="post">
-  <input type="submit" name="salir" value="Salir">
-</form>
-<br>
-<h3><u>Datos Personales</u></h3>
-<form action="v_modificarUsuario.php" method="post">
-  <input type="submit" name="modificar" value="Modificar">
-</form>
-<?php
-echo $objVerLog->objUser->getUsername() . "<br>";
-echo $objVerLog->objUser->getPassword() . "<br>";
-echo $objVerLog->objUser->getEmail() . "<br>";
-echo $objVerLog->objUser->getName() . " " . $objVerLog->objUser->getSurname() . "<br>";
-echo $objVerLog->objUser->getBirthday() . "<br>";
-echo $objVerLog->objUser->getAddress() . ", " . $objVerLog->objUser->getPostal() . " " . $objVerLog->objUser->getTown() . " " . $objVerLog->objUser->getProvince() . "<br>";
-echo $objVerLog->objUser->getMobile() . "<br>";
-echo $objVerLog->objUser->getTelephone() . "<br>";
+  require_once('../controller/c_mostrarUsuarios.php');
+
+  $objetoPerfil = new Perfil($_POST['username']=null,$_POST['password']=null);
+  $objetoPerfil->checkConexion();
+  
+  if(isset($_POST["boton_salir"])){
+    $objetoPerfil->closeSession();
+  }
+  
+  $objetoModificar =new ModificarDatos();
+  
+
+  if(isset($_POST["actualizar"])){
+    $objetoModificar->checkModificar();
+  }
+
+  $objetoFoto= new SubirFoto();
+
+  if (isset($_POST["Enviar_Imagen"])) {
+
+    $objetoFoto->foto();
+  }
+
+  $objetoMostrar = new MostrarUsuario();
+  //$arrayMostrar=$objetoMostrar->mostrar(); 
+
+  if (isset($_POST['insertar_nuevo'])) {
+    $objetoMostrar->addUser($_POST['Usu'],$_POST['Email'],$_POST['Tipo'],$_POST['Estado']);
+  }
 
 
-if ($_SESSION['idTipoUsuario']==1) {
+ ?>
 
-?>
-  <br>
-  <p>Vamos por buen camino. Tipo 1 </p>
-  <h4><u>Usuarios</u></h4>
-  <input type="button" onclick=" location.href='v_admin.php' " value="Funciones">
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Perfil</title>
+	<link rel="icon" type="image/jpeg" sizes="16x16" href="images/goku1.jpeg">
+    <link rel="stylesheet" type="text/css" href="css/perfil.css"> 
+</head>
+<body>
 
-<?php } elseif ($_SESSION['idTipoUsuario']==2) { ?>
-  <br>
-  Seguimos por buen caminio. Tipo 2 <br>
-  <h4><u>Curriculum</u></h4>
-  <form action='v_curriculum.php' method='post'>
-    <select class='lista' name='curriculum'>
-      <option value="0">Seleccione</option>
-        <?php
-          //  $objVerLog->listar();
+    <header>
+    <form action="<?PHP $PHP_SELF ?>" method="post">
+        <nav id="menu_gral">
+      	<ul id="menu">
+      		<li><a href=""><input class="btnsBarra" type="submit" name="boton_inicio" value="Inicio"></a></li>
+
+      		<?php 
+
+            if ($_SESSION['tipoUsuario']=='administrador') {
+              include('v_perfil_admin.php');
+            } elseif ($_SESSION['tipoUsuario']=='usuario') {
+              include('v_perfil_usuario.php');
+            } else {
+              include('v_perfil_consultor.php');
+            }
+            
+          ?>
+      		<li><a href=""><input class="btnsBarra" type="submit" name="boton_perfil" value="Perfil"></a>
+      			<ul>
+              <li><a href=""><input class="btn" type="submit" name="boton_foto" value="Subir Foto"></a></li>
+      				<li><a href=""><input class="btn" type="submit" name="boton_modificar" value="Modificar Datos"></a></li>
+      				<li><a href=""><input class="btn" type="submit" name="boton_salir" value="Logout"></a></li>
+      			</ul>
+      		</li>
+      	</ul>
+      </nav>
+      </form>
+    </header>
+
+    <div>
+    	<hr>
+    	<h2>Perfil <?php echo $_SESSION['username']?> </h2>
+      
+      <?php  
+
+      if (isset($_POST["boton_usuarios"])) {
+           //include('../controller/c_mostrarUsuarios.php');
+           include('v_mostrarUsuario.php');
+         }  
+
+      elseif(isset($_POST["boton_foto"])){  ?>
+
+        <form action="v_perfil.php" method="post" enctype="multipart/form-data">            
+         <?php include('v_subirFoto.php');       
          ?>
-    </select>
-    <input type='submit' name='submit' value='Ver'class='btn btn-success'/>
-  </form>
-  <input type="button" onclick=" location.href='v_curriculum.php' " value="Añadir">
-  <input type="button" onclick=" location.href='v_curriculum.php' " value="Modificar">
-  <input type="button" onclick=" location.href='v_curriculum.php' " value="Borrar">
-  <h4><u>Datos Académicos</u></h4>
-  <form action='v_datosAcademicos.php' method='post'>
-    <input type="submit" name="anadirAca" value="Añadir">
-    <input type="submit" name="modificarAca" value="Modificar">
-    <input type="submit" name="borrarAca" value="Borrar">
-  <h4><u>Datos Profesionales</u></h4>
-</form>
-  <form action='v_datosProfesionales.php' method='post'>
-    <input type="submit" name="anadirPro" value="Añadir">
-    <input type="submit" name="modificarPro" value="Modificar">
-    <input type="submit" name="borrarPro" value="Borrar">
-  </form>
-  <h4><u>Otros Datos</u></h4>
-  <form action='v_otrosDatos.php' method='post'>
-    <input type="submit" name="anadirOtro"  value="Añadir">
-    <input type="submit" name="modificarOtro"  value="Modificar">
-    <input type="submit" name="borrarOtro"  value="Borrar">
-  </form>
-<?php } else { ?>
-  <br>
-  Podemos hacer el cambio. Tipo 3 <br>
-  <h4><u>Buscador</u></h4>
-  <input type="button" onclick=" location.href='v_modificarUsuario.php' " value="Ver">
+        </form> 
+         <?php
+        } 
 
-<?php }  ?>
+       elseif(isset($_POST["boton_modificar"])){
+        ?>
+        <form action="v_perfil.php" method="post">            
+         <?php include('v_modificarDatos.php'); ?>
+        </form> 
+         <?php
+        } else {
+            echo $objetoPerfil->objetoUsuario->getRegistro() . "<br>"; 
+            echo "<br>";
+            echo $objetoPerfil->objetoUsuario->getName() . " " . $objetoPerfil->objetoUsuario->getSurname() . "<br>";
+            echo $objetoPerfil->objetoUsuario->getEmail() . "<br>";
+            echo $objetoPerfil->objetoUsuario->getDNI() . "<br>"; 
+            echo $objetoPerfil->objetoUsuario->getBirthday() . "<br>";
+            echo $objetoPerfil->objetoUsuario->getAddress() . ", " . $objetoPerfil->objetoUsuario->getPostal() . " " . $objetoPerfil->objetoUsuario->getTown() . " " . $objetoPerfil->objetoUsuario->getProvince() . "<br>";
+            echo $objetoPerfil->objetoUsuario->getMobile() . "<br>";
+            echo $objetoPerfil->objetoUsuario->getTelephone() . "<br>";
+
+       }
+
+      ?>
+    </div>
+
+    <footer>
+		<hr>
+		Copyright &copy; <?php echo date("Y"); ?>
+	</footer>
+    
+	
+</body>
+</html>

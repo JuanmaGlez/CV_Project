e15055815a5047d3f9e70e67619f5238e0eef6b1
JuanmaGlez <smigol_82@yahoo.es>
2017-05-26 20:28:37 +0200

    actualizado 20170512

--- view/v_registrar.php
+++ view/v_registrar.php
@@ -1,47 +1,146 @@
-<?php
-require_once ("/var/www/html/jobsnetworks/CV_Project/controller/c_registrar.php");
-if(isset($_POST["submit"])){
-  $objregistar=new Registrar();
-  $objregistar->checkUsuario();
-}
-?>
+<?php 
+
+    require_once("../controller/c_registrar.php");
+    
+    if (isset($_POST["submit"])) {
+
+        $objetoRegistrar=new Registrar(); 
+        $objetoRegistrar->insertar();
+    }
+
+ ?>
 
 <!DOCTYPE HTML>
 <html lang="es">
     <head>
         <meta charset="utf-8"/>
-        <title>Registrar</title>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <style>
-            input{
-                margin-top:5px;
-                margin-bottom:5px;
-            }
-            .right{
-                float:right;
-            }
-        </style>
+        <title>Registrar</title>        
+        <link rel="icon" type="image/jpeg" sizes="16x16" href="images/goku1.jpeg">
+        <link rel="stylesheet" type="text/css" href="css/registrar.css">    
     </head>
     <body>
         <form action="<?PHP $PHP_SELF ?>" method="post" class="col-lg-5">
-            <h4>Registrar Usuario</h4>
+            <h1>Registrar Usuario</h1>
             <hr/>
-            *Username: <input type="text" name="username" class="form-control"/> <!--required/>-->
-            *Password: <input type="password" name="password" class="form-control"/> <!--required/>-->
-            *Email: <input type="text" name="email" class="form-control"/><!--required />-->
-            *Name: <input type="text" name="name" class="form-control"/> <!--required/>-->
-            *Surname: <input type="text" name="surname" class="form-control"/> <!--required/>-->
-            *Birthday: <input type="text" name="birthday" class="form-control"/> <!--required/>-->
-            *Address: <input type="text" name="address" class="form-control"/> <!--required/>-->
-            *Postal: <input type="text" name="postal" class="form-control"/> <!--required/>-->
-            *Town: <input type="text" name="town" class="form-control"/> <!--required/>-->
-            *Province: <input type="text" name="province" class="form-control"/> <!--required/>-->
-            *Mobile: <input type="text" name="mobile" class="form-control"/> <!--required/>-->
-            Telephone: <input type="text" name="telephone" class="form-control"/>
-            Todos los campos con <strong>*</strong> son obligatorios
-            <input type="submit" name="submit" value="Submit" class="btn btn-success"/>
+            <table>
+                <tr>
+                    <td class="der">
+                        *Username:
+                    </td>
+                    <td>
+                        <input type="text" name="username" class="form-control"/> <!--required/>-->         
+                    </td>
+                </tr>
+                <tr>
+                    <td class="der">
+                        *Password:            
+                    </td>
+                    <td>
+                        <input type="password" name="password" class="form-control"/> <!--required/>-->
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *Email:             
+                    </td>
+                    <td>
+                        <input type="text" name="email" class="form-control"/><!--required />-->
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *Name:           
+                    </td>
+                    <td>
+                         <input type="text" name="name" class="form-control"/> <!--required/>--> 
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *Surname:                        
+                    </td>
+                    <td>
+                         <input type="text" name="surname" class="form-control"/> <!--required/>-->            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *DNI: 
+                    </td>
+                    <td>
+                        <input type="text" name="dni">            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *Birthday:
+                    </td>
+                    <td>
+                        <input type="text" name="birthday" class="form-control"/> <!--required/>-->            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *Address: 
+                    </td>
+                    <td>
+                        <input type="text" name="address" class="form-control"/> <!--required/>-->            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *Postal: 
+                    </td>
+                    <td>
+                        <input type="text" name="postal" class="form-control"/> <!--required/>-->            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *Town:
+                    </td>
+                    <td>
+                        <input type="text" name="town" class="form-control"/> <!--required/>-->            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *Province:
+                    </td>
+                    <td>
+                        <input type="text" name="province" class="form-control"/> <!--required/>-->            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        *Mobile:
+                    </td>
+                    <td>
+                        <input type="text" name="mobile" class="form-control"/> <!--required/>-->            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Telephone:
+                    </td>
+                    <td>
+                         <input type="text" name="telephone" class="form-control"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Todos los campos con <strong>*</strong> son obligatorios            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" name="submit" value="Enviar" class="btn btn-success"/>            
+                    </td>
+                    <td>
+                        <input type="reset" name="submit" value="Borrar" class="btn btn-success"/>            
+                    </td>
+                </tr>
+            </table>                       
         </form>
 
         <footer class="col-lg-12">

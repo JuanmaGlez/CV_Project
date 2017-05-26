e2db03030e1eade8edf61148f508265ea3166bc4
JuanmaGlez <smigol_82@yahoo.es>
2017-05-26 20:29:48 +0200

    nuevo 20170519

---
+++ view/v_perfil_consultor.php
@@ -0,0 +1,28 @@
+<li><a href="">Buscar</a>
+      <ul>
+            <li><a href="">Añadir Curriculum</a></li>
+            <li><a href="">Listar Curriculums</a></li>
+            <li><a href="">Mostrar Curriculum</a></li>
+      </ul>
+</li>
+<li><a href="">Formación</a>
+      <ul>
+            <li><a href="">Añadir Formación</a></li>
+            <li><a href="">Mostrar Formación</a></li>
+            <li><a href="">Modificar Formación</a></li>
+      </ul>
+</li>
+<li><a href="">Profesión</a>
+      <ul>
+            <li><a href="">Añadir Profesión</a></li>
+            <li><a href="">Mostrar Profesión</a></li>
+            <li><a href="">Modificar Profesión</a></li>
+      </ul>
+</li>
+<li><a href="">Otros</a>
+      <ul>
+            <li><a href="">Añadir Otros</a></li>
+            <li><a href="">Mostrar Otros</a></li>
+            <li><a href="">Modificar Otros</a></li>
+      </ul>
+</li>

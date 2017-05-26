b2ef58c51114f81e67355542e1b41f9556f0d764
JuanmaGlez <smigol_82@yahoo.es>
2017-05-26 20:29:54 +0200

    nuevo 20170519

---
+++ view/v_perfil_usuario.php
@@ -0,0 +1,25 @@
+<li><a href=""><input class="btnsBarra" type="submit" name="boton_curriculum" value="Curriculum"></a>
+      <ul>
+            <li><a href=""><input class="btn" type="submit" name="boton_addCurri" value="Añadir Curriculum"></a></li>
+            <li><a href=""><input class="btn" type="submit" name="boton_listCurri" value="Listar Curriculum"></a></li>
+            <li><a href=""><input class="btn" type="submit" name="boton_mostrarCurri" value="Mostrar Curriculum"></a></li>
+      </ul>
+</li>
+<li><a href=""><input class="btnsBarra" type="submit" name="boton_formacion" value="Formación"></a>
+      <ul>
+            <li><a href=""><input class="btn" type="submit" name="boton_addFormacion" value="Añadir Formación"></a></li>
+            <li><a href=""><input class="btn" type="submit" name="boton_mostrarFormacion" value="Mostrar Formación"></a></li>            
+      </ul>
+</li>
+<li><a href=""><input class="btnsBarra" type="submit" name="boton_profesion" value="Profesión"></a>
+      <ul>
+            <li><a href=""><input class="btn" type="submit" name="boton_addProfesion" value="Añadir Profesión"></a></li>
+            <li><a href=""><input class="btn" type="submit" name="boton_mostrarProfesion" value="Mostrar Profesión"></a></li>            
+      </ul>
+</li>
+<li><a href=""><input class="btnsBarra" type="submit" name="boton_otros" value="Otros"></a>
+      <ul>
+            <li><a href=""><input class="btn" type="submit" name="boton_addOtros" value="Añadir Otros"></a></li>
+            <li><a href=""><input class="btn" type="submit" name="boton_mostrarOtros" value="Mostrar Otros"></a></li>            
+      </ul>
+</li>

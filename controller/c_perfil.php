8cdccba5968390bf89c0547fbe3e2987d668f1f9
JuanmaGlez <smigol_82@yahoo.es>
2017-06-05 21:30:10 +0200

    actualizado 20170605

--- controller/c_perfil.php
+++ controller/c_perfil.php
@@ -1,89 +1,50 @@
 <?php 
 
-	session_start();
-	// Llamamos a la Clase Usuario
-	require_once('../model/Usuarios.php');
+	require_once("../model/m_perfil.php");
 
-	// Clase Perfil, donde se verifica la password del usuario entre otras cosas
-	class Perfil {
+	require_once("../model/m_modificarDatos.php");
 
-		private $usuario;
-		private $contrasena;
-		public $objetoUsuario;
-		
+	require_once("../model/m_subirFoto.php");
 
-		// Método Constructor de la Clase Perfil		
-		public function __construct($usuario,$contrasena){
-			$this->usuario=$usuario;
-			$this->contrasena=$contrasena;			
-			//$this->objetoUsuario=new Usuarios($_SESSION['idUsuario']);
-			if (empty($_SESSION['idUsuario'])) {
-				$this->objetoUsuario=new Usuarios();
-			} else {
-				$this->objetoUsuario=new Usuarios($_SESSION['idUsuario']);
-			}
+  	require_once('../model/m_mostrarUsuarios.php');	
 
-		} // Fin Constructor
+  	require_once('../model/m_perfil_consultor.php');
 
-		// Método checkLogin
-		public function checKLogin() {
-			$datosarray=$this->objetoUsuario->buscarUsuario($this->usuario);
-			if ($datosarray != 0) {				
-				
-				if ($datosarray['estado'] == 'activado') {
-					if (password_verify($this->contrasena, $datosarray['password'])) {
-						$_SESSION['idUsuario']=$datosarray['idUsuario'];
-						$_SESSION['username']=$datosarray['username'];
-						$_SESSION['pass']=$this->contrasena;
-						$_SESSION['tipoUsuario']=$datosarray['tipoUsuario'];
-						$_SESSION['loggedin'] = true;
-						$_SESSION['start'] = time();
-		        		$_SESSION['expire'] = $_SESSION['start'] + (10 * 60);
-						echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../view/v_perfil.php\">";
-					} else {
-						echo "Usuario o Contraseña son incorrectos.";
-					} // Fin Condición verificar password
-				} else {
-					echo "Este usuario esta desactivado.";
-				} // Fin Condición estado
-			} else {
-				echo "Usuario no existe";
-			}
-		} // Fin método checkLogin
+  	$objetoPerfil = new Perfil($_POST['username']=null,$_POST['password']=null);
+  
+  	$objetoPerfil->checkConexion();
+  
+  if(isset($_POST["boton_salir"])){
+    $objetoPerfil->closeSession();
+  }
+  
+  $objetoModificar =new ModificarDatos();
+  
 
-		public function guardarLogin($usuario) {
-			if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-				setcookie("nombre_usuario", "$usuario", time()+86400);
-			}
-		}
-
-		public function checkConexion(){
-			if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-		    } else {
-		      echo "Solo usuarios registrados.<br>";
-		      echo "<br><a href='../view/v_login.php'>Login</a>";
-		    exit;
-		    }
-
-		    $ahora = time();
-
-		    if ($ahora > $_SESSION['expire']) {
-		      session_destroy();
-		      //echo "Su sesión ha terminado. <a href='../view/v_login.php'>Volver a Entrar</a>";
-		      echo "Su sesión ha terminado. <a href='../view/v_login.php'><input type='button' name='salir' value='Login'></a>";
-		      //echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../view/v_login.php\">";
-		      exit;
-		    }
+  if(isset($_POST["actualizar"])){
+    $objetoModificar->checkModificar();
   }
 
-		// Método cerrar sesión
-		public function closeSession(){    
-		    unset ($_SESSION['username']);
-		    session_destroy();
-		    echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=../\">";
-		    exit;
-		} //Fin método cerrar sesión
+  $objetoFoto= new SubirFoto();
 
+  if (isset($_POST["Enviar_Imagen"])) {
 
-	} // Fin de la Clase Perfil
+    $objetoFoto->foto();
+  }
+
+  $objetoMostrar = new MostrarUsuario();
+  //$arrayMostrar=$objetoMostrar->mostrar(); 
+
+  if (isset($_POST['insertar_nuevo'])) {
+    $objetoMostrar->addUser($_POST['Usu'],$_POST['Email'],$_POST['Tipo'],$_POST['Estado']);
+  }
+
+  $objetoFiltrar=new Filtrarlos();
+  
+  if (isset($_POST['envio_filtrar'])) {
+    $objetoFiltrar->mostrarFiltro();  
+  }
+
+	require_once("../view/v_perfil.php");
+
  ?>

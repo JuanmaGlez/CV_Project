e989c1a590a6563086e10da0cbeb6e20fa52d374
JuanmaGlez <smigol_82@yahoo.es>
2017-06-05 21:29:48 +0200

    actualizado 20170605

--- controller/c_mostrarUsuarios.php
+++ controller/c_mostrarUsuarios.php
@@ -1,54 +1,18 @@
 <?php 
 
-	/*require_once('../model/Usuarios.php');
-	$objetoUsuario = new Usuarios();
+	require_once("../view/v_perfil_admin.php");
 
-	$arrayPersonas = $objetoUsuario->getUsuarios();	
-	 if(isset($_POST['boton_desactivar'])){
-                $objetoUsuario->desactivarCuenta($arrayPersonas['idUsuario']);
-              } 
+	require_once("../model/m_mostrarUsuarios.php");
 
-	require_once('../view/v_mostrarUsuario.php');*/
+	$objetoMostrar = new MostrarUsuario();
 
-	require_once("../model/Usuarios.php");
+	if(isset($_POST["boton_usuarios"])){
 
-	class MostrarUsuario {
+		
+  		$arrayMostrar=$objetoMostrar->mostrar();
 
-		private $objetoUsuario;
+  	}
 
-		public function __construct() {
-			$this->objetoUsuario = new Usuarios();
-		}
-
-		public function mostrar() {
-			$arrayPersonas=$this->objetoUsuario->getUsuarios();
-			return $arrayPersonas;
-		}
-
-		public function desactivar($idUsuario) {
-			$this->objetoUsuario->desactivarCuenta($idUsuario);
-		}
-
-		public function activar($idUsuario) {
-			$this->objetoUsuario->activarCuenta($idUsuario);
-		}
-
-		public function addUser($username, $email, $tipoUsuario, $estado) {
-			$contrasenia=$username;
-			$password=password_hash($contrasenia, PASSWORD_DEFAULT, array("cost"=>12));
-			$dni=$username;
-			$resultado=$this->objetoUsuario->addUser($username, $password, $email, $dni, $tipoUsuario, $estado);
-
-			if  ($resultado==2) {
-				echo "usuario registrado";
-			} elseif ($resultado==1) {
-				echo "Faltan campos por rellenar";
-			} else {
-				echo "El usuario ya existe";
-			
-			}
-		}
-
-	} // Fin de la clase
+	require_once("../view/v_mostrarUsuario.php");
 
  ?>

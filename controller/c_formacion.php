9ae476d76907734dbeb95d2acaa35f1eab6635df
JuanmaGlez <smigol_82@yahoo.es>
2017-06-07 21:36:06 +0200

    Nuevo 2017/06/07

---
+++ controller/c_formacion.php
@@ -0,0 +1,11 @@
+<?php 
+
+	require_once("../view/v_perfil_usuario.php");	
+
+	require_once("../model/m_formacion.php");
+
+	$objetoMostrar = new Formax();
+
+	require_once("../view/v_formacion.php");
+
+ ?>
